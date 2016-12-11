97db4f03b08bda044bbf7c7ed257dc98e4d955dc
Jason <jlmerri1@neiu.edu>
2016-12-11 07:21:25 -0600

    add header image

--- JackedUpChicago/JackedUpChicago/MasterPage.Master.designer.cs
+++ JackedUpChicago/JackedUpChicago/MasterPage.Master.designer.cs
@@ -31,6 +31,15 @@ namespace JackedUpChicago {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Imagecoffeebeans control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Imagecoffeebeans;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
